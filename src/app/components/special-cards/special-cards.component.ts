67399112eb5c9ca5b28338292f307afcf58e3925
gagangayari <gagangayari@gmail.com>
2024-03-03 16:51:12 +0530

    Made changes for images in special items

--- src/app/components/special-cards/special-cards.component.ts
+++ src/app/components/special-cards/special-cards.component.ts
@@ -11,23 +11,26 @@ import { MenuItemDescComponent } from '../menu-item-desc/menu-item-desc.componen
 export class SpecialCardsComponent {
 
   @Input() itemName = '';
-  @Input() imgUrl: string = "../assets/alex-haney-CAhjZmVk5H4-unsplashLarge.jpeg";
+  // @Input() imgUrl: string = "../assets/alex-haney-CAhjZmVk5H4-unsplashLarge.jpeg";
 
   items : Item[]   = [
     {
       'itemName': 'Paneer Tikka',
       'desc': 'Embark on a tantalizing journey with Paneer Tikka, a vegetarian delight that ignites',
       'price': 200,
+      'imgUrl': "../assets/alex-haney-CAhjZmVk5H4-unsplashLarge.jpeg"
     },
     {
       'itemName': 'Chicken Kebab',
       'desc': 'Savor the sizzling delight of Chicken Kebab, where succulent pieces of marinated chicken are expertly grilled to perfection.',
       'price': 400,
+      'imgUrl': "../assets/ali-inay-y3aP9oo9Pjc-unsplashLarge.jpeg"
     },
     {
       'itemName': 'Chicken Biryani',
       'desc': 'Indulge in the regal flavors of Chicken Biryani, where succulent chicken and fragrant basmati rice unite in a harmonious dance of spices.',
       'price': 500,
+      'imgUrl': "../assets/jonathan-borba-uB7q7aipU2o-unsplashLarge.jpeg"
     }
   ]
 
